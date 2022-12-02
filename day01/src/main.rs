27f69e4c8f3821c66e08a61d0123e442ec24184a
Tristan Honscheid <tristan@tristantech.net>
2022-12-01 21:32:39 -0700

    Tweak Day 01 solution

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,25 +1,21 @@
 use std::collections::BinaryHeap;
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
+use std::fs;
 
 fn main() {
     // Read input from file
-    let mut file = File::open(Path::new("input.txt")).unwrap();
-    let mut text = String::new();
-    file.read_to_string(&mut text).unwrap();
+    let text = fs::read_to_string("input.txt").unwrap();
 
     // Use a max heap to track the top elves
     let mut heap: BinaryHeap<i32> = BinaryHeap::new();
 
     // For each elf, tally up how many calories they are carrying
     for elf in text.split("\n\n") {
-        heap.push(elf.split('\n').map(|c| c.parse::<i32>().unwrap_or(0)).sum())
+        heap.push(elf.lines().map(|c| c.parse::<i32>().unwrap()).sum())
     }
 
     println!("Max: {}", heap.peek().unwrap());
     println!(
         "Top 3: {}",
-        (0..3).fold(0, |sum, _| sum + heap.pop().unwrap())
+        (0..3).map(|_| heap.pop().unwrap()).sum::<i32>()
     );
 }
